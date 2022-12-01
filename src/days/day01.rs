b7e7fac3d2f26b022686a3646de57efb32e9914e
didi <didi@leaf.space>
2022-12-01 14:45:56 +0100

    day1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,9 +1,55 @@
 use crate::{Solution, SolutionPair};
+use std::fs::read_to_string;
+
+fn parse_input(path: &str) -> Vec<Vec<u64>> {
+    let input = read_to_string(path).unwrap();
+    input
+        .split("\n\n")
+        .into_iter()
+        .map(|s| {
+            s.split_whitespace()
+                .into_iter()
+                .map(|item| item.to_string().parse::<u64>().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn part_one(input: &Vec<Vec<u64>>) -> u64 {
+    input
+        .iter()
+        .map(|sub| sub.iter().sum::<u64>())
+        .max()
+        .unwrap()
+}
+
+fn part_two(input: &Vec<Vec<u64>>) -> u64 {
+    let mut asd = input
+        .iter()
+        .map(|sub| sub.iter().sum::<u64>())
+        .collect::<Vec<u64>>();
+    asd.sort();
+    let len = asd.len();
+    asd[len - 3..len].into_iter().sum()
+}
 
 pub fn solve() -> SolutionPair {
     // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = parse_input("input/day01_input.txt");
+    let sol1: u64 = part_one(&input);
+    let sol2: u64 = part_two(&input);
 
     (Solution::U64(sol1), Solution::U64(sol2))
+}
+
+#[test]
+fn test_part_one() {
+    let input = parse_input("input/day01_test.txt");
+    assert_eq!(part_one(&input), 24000);
+}
+
+#[test]
+fn test_part_two() {
+    let input = parse_input("input/day01_test.txt");
+    assert_eq!(part_two(&input), 45000);
 }
