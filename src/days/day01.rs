49211d0de7baf1dc77493b9466e9e72c52bf2786
didi <didi@leaf.space>
2022-12-01 11:28:17 +0100

    first commit setup following good https://github.com/agubelu/AoC-rust-template

---
+++ src/days/day01.rs
@@ -0,0 +1,9 @@
+use crate::{Solution, SolutionPair};
+
+pub fn solve() -> SolutionPair {
+    // Your solution here...
+    let sol1: u64 = 0;
+    let sol2: u64 = 0;
+
+    (Solution::U64(sol1), Solution::U64(sol2))
+}
